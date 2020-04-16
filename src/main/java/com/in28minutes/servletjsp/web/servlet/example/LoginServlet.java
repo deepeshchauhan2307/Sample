f4f0fee23f29eb7466f88ca2f799aa1eaa4eeab9
Deepesh Chauhan <deepesh.chauhan@nagarro.com>
2020-04-16 13:27:22 +0530

    adding files

--- src/main/java/com/in28minutes/servletjsp/web/servlet/example/LoginServlet.java
+++ src/main/java/com/in28minutes/servletjsp/web/servlet/example/LoginServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/login")
+@WebServlet(urlPatterns = "")
 public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request,
@@ -29,7 +29,7 @@ public class LoginServlet extends HttpServlet {
 
 		if (email.equalsIgnoreCase("user@google.com")
 				&& password.equalsIgnoreCase("password")) {
-			request.getRequestDispatcher("/first").forward(request, response);
+			request.getRequestDispatcher("/WEB-INF/views/first.jsp").forward(request, response);
 		} else {
 			request.setAttribute("error", "Invalid ID/Password");
 			request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(
