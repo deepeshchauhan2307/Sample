42f82f325e2d64f89367cb27725d2b3f40ecdab7
Deepesh Chauhan <deepesh.chauhan@nagarro.com>
2020-04-16 14:09:00 +0530

    adding files

--- src/main/java/com/in28minutes/servletjsp/web/servlet/example/FirstServlet.java
+++ src/main/java/com/in28minutes/servletjsp/web/servlet/example/FirstServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "")
+@WebServlet(urlPatterns = "/first")
 public class FirstServlet extends HttpServlet {
 	// FirstServlet
 	// LoginServlet
